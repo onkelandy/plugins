e6bb7ecc54eee9053c7f9d3fb957732333d9d392
Morg42 <43153739+Morg42@users.noreply.github.com>
2023-08-16 12:25:36 +0200

    se: small fixes, test/disable visualization

--- stateengine/StateEngineItem.py
+++ stateengine/StateEngineItem.py
@@ -1259,13 +1259,13 @@ class SeItem:
             except Exception as e:
                 self.__logger.error("struct {} creation failed. Error: {}", item_id, e)
             if item is None:
-            return item
                 self.__logger.warning("Item '{0}' not found!", item_id)
+            return item
         if not item_id.startswith("."):
             item = self.itemsApi.return_item(item_id)
             if item is None:
-            return item
                 self.__logger.warning("Item '{0}' not found!", item_id)
+            return item
         self.__logger.debug("Testing for relative item declaration {}", item_id)
         parent_level = 0
         for c in item_id:
@@ -1288,8 +1288,8 @@ class SeItem:
         if item is None:
             self.__logger.warning("Determined item '{0}' does not exist.", result)
         else:
-        return item
             self.__logger.develop("Determined item '{0}' for id {1}.", item.id, item_id)
+            return item
 
     # Return an item related to the StateEngine object item
     # attribute: Name of the attribute of the StateEngine object item, which contains the item_id to read
